91c476cde8f0a1f0e98f40e34cc0cf51423ba61f
EC2 Default User <ec2-user@ip-172-31-16-203.ap-northeast-1.compute.internal>
2023-11-19 15:54:27 +0000

    Initial Commit

---
+++ db/migrate/20231119153405_create_reservations.rb
@@ -0,0 +1,19 @@
+class CreateReservations < ActiveRecord::Migration[6.1]
+  def change
+    create_table :reservations do |t|
+
+      t.string :group_name, null: false
+      t.integer :num_children, null: false, default: 0
+      t.integer :num_students, null: false, default: 0
+      t.integer :num_adults, null: false, default: 0
+      t.integer :num_games, null: false
+      t.integer :num_lanes, null: false
+      t.date :start_date, null: false
+      t.time :start_time, null: false
+      t.text :note
+      t.integer :games_fee, null: false
+      t.boolean :is_active, null: false, default: true
+      t.timestamps
+    end
+  end
+end
