9a2236097e4348da3a14b09f244478f2a627ac6a
EC2 Default User <ec2-user@ip-172-31-16-203.ap-northeast-1.compute.internal>
2023-11-22 13:39:28 +0000

    Finish user login

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :reservations, dependent: :destroy
+
+  katakana = /\A[\p{katakana}\u{30fc}]+\z/
+
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true, format: {with: katakana, message: 'はカタカナで入力して下さい。'}
+  validates :first_name_kana, presence: true, format: {with: katakana, message: 'はカタカナで入力して下さい。'}
+  validates :postal_code, presence: true
+  validates :address, presence: true
+  validates :telephone_number, presence: true
+  validates :email, presence: true
 end
