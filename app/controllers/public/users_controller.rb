bf973b50475d3c3cba875d1711eea7afe04105b3
EC2 Default User <ec2-user@ip-172-31-16-203.ap-northeast-1.compute.internal>
2023-11-23 16:56:34 +0000

    Finish public/user

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -8,5 +8,17 @@ class Public::UsersController < ApplicationController
   end
 
   def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to my_page_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email)
   end
 end
