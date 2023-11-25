ea2c86165bab1c23007d23dc6a17948d2dfb77f9
EC2 Default User <ec2-user@ip-172-31-16-203.ap-northeast-1.compute.internal>
2023-11-25 13:09:14 +0000

    Create user_index

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,8 +1,10 @@
 class Admin::UsersController < ApplicationController
   def index
+    @users = User.page(params[:page])
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
