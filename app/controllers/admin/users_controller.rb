b1e605f711bf7d4981515d621dbb28f4d9ee49ba
EC2 Default User <ec2-user@ip-172-31-16-203.ap-northeast-1.compute.internal>
2023-11-25 19:06:44 +0000

    Finish admin_user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,11 +5,25 @@ class Admin::UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @reservations = @user.reservations.order(created_at: :desc).first(5)
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user.id)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email)
   end
 end
--- app/controllers/public/reservations_controller.rb
+++ app/controllers/public/reservations_controller.rb
@@ -1,5 +1,6 @@
 class Public::ReservationsController < ApplicationController
   def new
+    @reservation = Reservation.new
   end
 
   def select_time
@@ -24,5 +25,11 @@ class Public::ReservationsController < ApplicationController
   end
 
   def cancel
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:group_name, :num_children, :num_students, :num_adults, :num_games, :num_lanes, :start_date, :start_time, :note, :games_fee)
   end
 end
