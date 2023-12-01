1a764f35bf90869b1b91132096815902f37506ad
EC2 Default User <ec2-user@ip-172-31-16-203.ap-northeast-1.compute.internal>
2023-12-01 15:54:44 +0000

    Finish admin_reservation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,3 +1,12 @@
 class Reservation < ApplicationRecord
   belongs_to :user
+
+  validates :group_name, presence: true
+  validates :num_children, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+  validates :num_students, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+  validates :num_adults, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+  validates :num_games, presence: true
+  validates :num_lanes, presence: true
+  validates :start_date, presence: true
+  validates :start_time, presence: true
 end
