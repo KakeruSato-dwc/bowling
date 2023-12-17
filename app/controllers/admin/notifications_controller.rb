7fef00bd80130d1f617e8516afea5e0a06738623
EC2 Default User <ec2-user@ip-172-31-16-203.ap-northeast-1.compute.internal>
2023-12-17 15:43:27 +0000

    Create save_notification_system

--- app/controllers/admin/notifications_controller.rb
+++ app/controllers/admin/notifications_controller.rb
@@ -1,4 +1,8 @@
 class Admin::NotificationsController < ApplicationController
   def index
+    @notifications = Notification.all.order(created_at: :desc)
+    @notifications.where(checked: false).each do |notification|
+      notification.update(checked: true)
+    end
   end
 end
