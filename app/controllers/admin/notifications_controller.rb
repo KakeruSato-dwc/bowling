b552964ee0dd940f40a4772aa54d1e68001aafee
EC2 Default User <ec2-user@ip-172-31-16-203.ap-northeast-1.compute.internal>
2024-01-01 15:53:54 +0000

    Add num_new_notifications

--- app/controllers/admin/notifications_controller.rb
+++ app/controllers/admin/notifications_controller.rb
@@ -1,6 +1,7 @@
 class Admin::NotificationsController < ApplicationController
   def index
     @notifications = Notification.order(created_at: :desc).page(params[:page]).per(10)
+    @num_new_notifications = @notifications.where(checked: false).count
     @notifications.where(checked: false).each do |notification|
       notification.update(checked: true)
     end
