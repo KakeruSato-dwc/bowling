e57e627be0cf47365b1143d38f3ad89f98e670c2
EC2 Default User <ec2-user@ip-172-31-16-203.ap-northeast-1.compute.internal>
2024-01-05 13:20:44 +0000

    Fix ja.yml

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -8,8 +8,8 @@ class Reservation < ApplicationRecord
   validates :num_children, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0}
   validates :num_students, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0}
   validates :num_adults, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0}
-  validates :num_games, presence: true
-  validates :num_lanes, presence: true
+  validates :num_games, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 1}
+  validates :num_lanes, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 1}
   validates :start_date, presence: true
   validates :start_time, presence: true, format: {with: /[1-2][0-9]:[0-5][0-9]/}
 end
