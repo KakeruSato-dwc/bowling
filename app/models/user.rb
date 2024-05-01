8603b1f67af3945d0a15e26cd03f7a12db288c59
EC2 Default User <ec2-user@ip-172-31-16-203.ap-northeast-1.compute.internal>
2024-05-01 08:02:12 +0000

    Add user_validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,8 +13,8 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name_kana, presence: true, format: {with: katakana, message: 'はカタカナで入力して下さい。'}
   validates :first_name_kana, presence: true, format: {with: katakana, message: 'はカタカナで入力して下さい。'}
-  validates :postal_code, presence: true
+  validates :postal_code, presence: true, numericality: {only_integer: true}
   validates :address, presence: true
-  validates :telephone_number, presence: true
+  validates :telephone_number, presence: true, numericality: {only_integer: true}
   validates :email, presence: true
 end
